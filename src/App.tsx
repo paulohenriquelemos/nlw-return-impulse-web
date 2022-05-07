b0686270dca12bf73866b08b8ca3be8941e7fe26
Paulo Henrique <ph-lemos@hotmail.com>
2022-05-07 20:48:28 -0300

    Aula 05

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+
+
+// npm create vite@latest
+
+import { Widget } from "./components/Widget";
+
+// npm install
+
+// npm run dev
+
+// npm install -D tailwindcss postcss autoprefixer
+
+// npx tailwindcss init -p
+
+// npm install phosphor-react
+
+// npm install @headlessui/react
+
+// npm install -D @tailwindcss/forms
+
+// npm install --save-dev tailwind-scrollbar
+
+// npm install html2canvas
+
+// npm install axios
+
+
+export function App() {
+
+  return (
+    <div>
+      <Widget />
+    </div>
+  )
+}
